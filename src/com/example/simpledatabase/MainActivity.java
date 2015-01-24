d33f802d1af6dfc13b6e15d1b108e50d58bc6d6d
kobake <kobake@user.sourceforge.net>
2015-01-24 15:44:32 +0900

    とりあえず生SQLに特化

--- src/com/example/simpledatabase/MainActivity.java
+++ src/com/example/simpledatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simpledatabase;
 
 import java.util.ArrayList;
 
+import android.R.integer;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -10,6 +11,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -18,10 +22,8 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	ListView mListView;
 	ArrayList<String> mData = new ArrayList<String>();
-	ArrayAdapter<String> mAdapter; 
-	
-	// DB結合用アダプタ
-	// SimpleCursorAdapter mAdapter2;
+	ArrayAdapter<String> mAdapter;
+	int mSelected = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +31,27 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		// widgets
 		mListView = (ListView)findViewById(R.id.listView1);
-		mData.add("AA");
-		mData.add("BB");
-		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mData);
+		// mData.add("AA");
+		// mData.add("BB");
+		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, mData);
 		mListView.setAdapter(mAdapter);
-		
-		// DB
-		// new SimpleCursorAdapter(this, )
+		mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		mListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				mSelected = position;			
+			}
+		});
+		clearResult();
+		select();
+	}
+	
+	// DBインターフェース取得
+	SQLiteDatabase getDb(){
+		// データベースファイルを開く。存在しなければ作って開く。
+		SQLiteDatabase db = this.openOrCreateDatabase("test.db", MODE_PRIVATE, null);
+		return db;
 	}
 	
 	// 結果表示
@@ -45,137 +61,131 @@ public class MainActivity extends Activity {
 	}
 	public void addResult(String result){
 		TextView textView = (TextView)findViewById(R.id.textView1);
-		textView.setText(textView.getText().toString() + "\n" + result);
+		textView.setText(textView.getText().toString() + result + "\n");
 	}
 	
-	// まずは挿入する
+	// 選択中のレコードのIDを取得
+	public int getSelectedRecordId(){
+		int i = mSelected; // mListView.getSelectedItemPosition();
+		if(i >= 0 && i < mData.size()){
+			return Integer.parseInt(mData.get(i).split(":")[0]);	
+		}
+		else{
+			return -1;
+		}
+	}
+	
+	// CREATE TABLE
+	public void buttonMethodCreate(View v){
+		clearResult();
+		SQLiteDatabase db = getDb();
+		try{
+			// db.execSQL("CREATE TABLE IF NOT EXISTS messages(_id INTEGER PRIMARY KEY AUTOINCREMENT, body VARCHAR)");
+			db.execSQL("CREATE TABLE messages(_id INTEGER PRIMARY KEY AUTOINCREMENT, body VARCHAR)");
+			addResult("CREATE TABLE 成功");
+		}
+		catch(Exception ex){
+			addResult("CREATE TABLE 失敗: " + ex.getMessage());
+		}
+		select();
+	}
+
+	// DROP TABLE
+	public void buttonMethodDrop(View v){
+		clearResult();
+		SQLiteDatabase db = getDb();
+		try{
+			// db.execSQL("DROP TABLE IF EXISTS messages");
+			db.execSQL("DROP TABLE messages");
+			addResult("DROP TABLE 成功");
+		}
+		catch(Exception ex){
+			addResult("DROP TABLE 失敗: " + ex.getMessage());
+		}
+	}
+	
+	// INSERT
 	public void buttonMethodInsert(View v){
 		clearResult();
-		MyDbHelper helper = new MyDbHelper(this);
-		SQLiteDatabase db = helper.getWritableDatabase();
+		SQLiteDatabase db = getDb();
 		// execSQLメソッド版
 		try{
 			db.execSQL("INSERT INTO messages(body) VALUES('abc')");
-			addResult("execSQL版が成功しました");
+			addResult("INSERT 成功");
 		}
 		catch(Exception ex){
-			addResult("execSQL版が失敗しました");
+			addResult("INSERT 失敗: " + ex.getMessage());
 		}
-		// insertメソッド版
-		try{
-			ContentValues values = new ContentValues();
-			values.put("body", "def");
-			db.insert(
-				"messages", // テーブル名
-				null,		// データを挿入する際にnull値が許可されていないカラムに代わりに利用される値
-				values		// 値群
-			);
-			addResult("insert版が成功しました");
-		}
-		catch(Exception ex){
-			addResult("insert版が失敗しました");
-		}
+		select();
 	}
 	
 	// SELECT
 	public void buttonMethodSelect(View v){
 		clearResult();
-		MyDbHelper helper = new MyDbHelper(this);
-		SQLiteDatabase db = helper.getWritableDatabase();
+		select();
+	}
+	public void select(){
+		SQLiteDatabase db = getDb();
 		
 		// raqQueryメソッド版
 		try{
+			mData.clear();
 			Cursor c = db.rawQuery("SELECT _id, body FROM messages", null);
 			while(c.moveToNext()){
 				int id = c.getInt(c.getColumnIndex("_id")); // ※ c.getString(0) と同じ
 				String body = c.getString(c.getColumnIndex("body")); // ※ c.getString(1) と同じ
-				addResult(id + ":" + body);
+				// addResult(id + ":" + body);
+				mData.add(id + ":" + body);
 			}
-			addResult("rawQuery版が成功しました");
+			mAdapter.notifyDataSetChanged();
+			addResult("SELECT 成功");
 		}
 		catch(Exception ex){
-			addResult("rawQuery版が失敗しました");
-		}
-		
-		// queryメソッド版
-		try{
-			Cursor c = db.query(
-				"messages",						// テーブル名
-				new String[]{"_id", "body"},	// 選択するカラム群
-				null,							// selection
-				null,							// selectionArgs
-				null,							// group by
-				null,							// having
-				null							// order by
-			);
-			while(c.moveToNext()){
-				int id = c.getInt(c.getColumnIndex("_id")); // ※ c.getString(0) と同じ
-				String body = c.getString(c.getColumnIndex("body")); // ※ c.getString(1) と同じ
-				addResult(id + ":" + body);
-			}
-			addResult("query版が成功しました");
-		}
-		catch(Exception ex){
-			addResult("query版が失敗しました");
+			addResult("SELECT 失敗: " + ex.getMessage());
 		}
 	}
 	
 	public void buttonMethodUpdate(View v){
 		clearResult();
-		MyDbHelper helper = new MyDbHelper(this);
-		SQLiteDatabase db = helper.getWritableDatabase();
-		
+		SQLiteDatabase db = getDb();
+
+		// 対象レコードID
+		int id = getSelectedRecordId();
+		if(id == -1){
+			addResult("項目が選択されていません");
+			return;
+		}
+
 		// execSQLメソッド版
 		try{
-			db.execSQL("UPDATE messages SET body = 'xyz' WHERE _id <= 2"); // _id = 1, 2 のレコードを更新
-			addResult("execSQL版が成功しました");
+			db.execSQL("UPDATE messages SET body = body || 'A' WHERE _id = " + id);
+			addResult("UPDATE 成功");
 		}
 		catch(Exception ex){
-			addResult("execSQL版が失敗しました");
+			addResult("UPDATE 失敗: " + ex.getMessage());
 		}
-		
-		// queryメソッド版
-		try{
-			ContentValues values = new ContentValues();
-			values.put("body", "XYZ");
-			db.update(
-				"messages",				// テーブル名
-				values,					// 値群。
-				"_id >=3 AND _id <= 4", // 条件。 _id = 3, 4 のレコードを更新
-				null // where args
-			);
-			addResult("update版が成功しました");
-		}
-		catch(Exception ex){
-			addResult("update版が失敗しました");
-		}
+		select();
 	}
 
 	public void buttonMethodDelete(View v){
 		clearResult();
-		MyDbHelper helper = new MyDbHelper(this);
-		SQLiteDatabase db = helper.getWritableDatabase();
+		SQLiteDatabase db = getDb();
+		
+		// 対象レコードID
+		int id = getSelectedRecordId();
+		if(id == -1){
+			addResult("項目が選択されていません");
+			return;
+		}
 		
 		// execSQLメソッド版
 		try{
-			db.execSQL("DELETE FROM messages WHERE _id <= 2"); // _id = 1, 2 のレコードを削除
-			addResult("execSQL版の削除が成功しました");
+			db.execSQL("DELETE FROM messages WHERE _id = " + id);
+			addResult("DELETE 成功");
 		}
 		catch(Exception ex){
-			addResult("execSQL版の削除が失敗しました");
+			addResult("DELETE 失敗: " + ex.getMessage());
 		}
-		
-		// queryメソッド版
-		try{
-			db.delete(
-				"messages",				// テーブル名
-				"_id >=3 AND _id <= 4", // 条件。 _id = 3, 4 のレコードを削除
-				null // where args
-			);
-			addResult("delete版の削除が成功しました");
-		}
-		catch(Exception ex){
-			addResult("delete版の削除が失敗しました");
-		}
+		select();
 	}
 }
